94be00c3aa73c9ef4a2780694467942be368e3c1
Baber Khan <bbrkhnn@outlook.com>
2024-06-26 21:29:58 +0100

    Add some responsiveness and add header, footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -89,75 +89,82 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <h1 className="w-full bg-zinc-900 p-5 text-center font-black text-zinc-200">
-        Book Search Engine
-      </h1>
-      <form className="flex w-full flex-row items-center justify-center gap-8 bg-zinc-300 p-5">
-        <div className="flex flex-row gap-1">
-          <input
-            id="author"
-            type="text"
-            placeholder="Author"
-            className="w-full rounded-l-full border border-zinc-500 px-6 py-4"
-            onChange={handleInputChange}
-            value={userPrompt.author}
-          />
-          <input
-            id="book-title"
-            type="text"
-            placeholder="Book Title"
-            className="w-full rounded-r-full border border-zinc-500 px-6 py-4"
-            onChange={handleInputChange}
-            value={userPrompt.bookTitle}
-          />
+    <>
+      <header>
+        <h1 className="w-full bg-zinc-900 p-5 text-center font-black text-zinc-200">
+          Book Search Engine
+        </h1>
+        <form className="flex w-full flex-col items-center justify-center gap-8 bg-zinc-300 p-5 md:flex-row">
+          <div className="flex flex-row gap-1">
+            <input
+              id="author"
+              type="text"
+              placeholder="Author"
+              className="w-full rounded-l-full border border-zinc-500 px-6 py-4"
+              onChange={handleInputChange}
+              value={userPrompt.author}
+            />
+            <input
+              id="book-title"
+              type="text"
+              placeholder="Book Title"
+              className="w-full rounded-r-full border border-zinc-500 px-6 py-4"
+              onChange={handleInputChange}
+              value={userPrompt.bookTitle}
+            />
+          </div>
+          <div className="flex flex-row gap-4">
+            <button
+              id="search"
+              className="w-fit rounded-full bg-zinc-800 px-10 py-4 font-bold text-zinc-200"
+              onClick={handleButtonClick}
+            >
+              Search
+            </button>
+            <button
+              id="clear-search"
+              className="underline"
+              onClick={handleButtonClick}
+            >
+              Clear Search
+            </button>
+          </div>
+        </form>
+      </header>
+      <main className="flex min-h-screen flex-col items-center">
+        <div className="flex flex-col items-center gap-12 px-6 py-12 md:p-12">
+          <h2 className="text-2xl font-bold">Results:</h2>
+          <div>
+            {isLoading ? (
+              <div>Search results loading...</div>
+            ) : (
+              <ul className="flex flex-row flex-wrap justify-center gap-8 md:gap-16">
+                {results.map((book) => {
+                  const id = uuidv4();
+                  const title = book.volumeInfo.title;
+                  const thumbnail =
+                    book.volumeInfo.imageLinks?.thumbnail || placeholderImage;
+                  const infoLink = book.volumeInfo.infoLink;
+                  return (
+                    <li key={id} className="flex w-[128px] flex-col gap-4">
+                      <img src={thumbnail} alt={`Thumbnail for ${title}`} />
+                      <a
+                        href={infoLink}
+                        className="h-12 overflow-hidden underline"
+                      >
+                        {title}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </div>
         </div>
-        <div className="flex flex-row gap-4">
-          <button
-            id="search"
-            className="w-fit rounded-full bg-zinc-800 px-10 py-4 font-bold text-zinc-200"
-            onClick={handleButtonClick}
-          >
-            Search
-          </button>
-          <button
-            id="clear-search"
-            className="underline"
-            onClick={handleButtonClick}
-          >
-            Clear Search
-          </button>
-        </div>
-      </form>
-      <div className="flex flex-col items-center gap-12 p-12">
-        <h2 className="text-2xl font-bold">Results:</h2>
-        <div>
-          {isLoading ? (
-            <div>Search results loading...</div>
-          ) : (
-            <ul className="flex flex-row flex-wrap justify-center gap-16">
-              {results.map((book) => {
-                const id = uuidv4();
-                const title = book.volumeInfo.title;
-                const thumbnail =
-                  book.volumeInfo.imageLinks?.thumbnail || placeholderImage;
-                const infoLink = book.volumeInfo.infoLink;
-                return (
-                  <li key={id} className="flex w-[128px] flex-col gap-4">
-                    <img src={thumbnail} alt={`Thumbnail for ${title}`} />
-                    <a
-                      href={infoLink}
-                      className="h-12 overflow-hidden underline"
-                    >
-                      {title}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          )}
-        </div>
-      </div>
-    </main>
+      </main>
+      <footer className="w-full bg-black p-4 text-center text-sm text-zinc-400">
+        Copyright (c) Baberr.com
+      </footer>
+    </>
   );
 }
